9d0822665099879d1a4d4b3ad0cb43a6597437e6
= <uppunuthula925@gmail.com>
2022-01-18 11:54:25 +0530

    version 1 (Added date and updated UI)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,61 @@
 setInterval(showTime, 1000);
 function showTime() {
   let time = new Date();
+  let date = time.getDate();
+  let month = time.getMonth();
   let hour = time.getHours();
   let minutes = time.getMinutes();
   let seconds = time.getSeconds();
   let meridian = "AM";
+  switch (month) {
+    case 0:
+      month = "Jan";
+      break;
+    case 1:
+      month = "Feb";
+      break;
+    case 2:
+      month = "Mar";
+      break;
+    case 3:
+      month = "Apr";
+      break;
+    case 4:
+      month = "May";
+      break;
+    case 5:
+      month = "Jun";
+      break;
+    case 6:
+      month = "Jul";
+      break;
+    case 7:
+      month = "Aug";
+      break;
+    case 8:
+      month = "Sept";
+      break;
+    case 9:
+      month = "Oct";
+      break;
+    case 10:
+      month = "Nov";
+      break;
+    default:
+      month = "Dec";
+  }
   meridian = hour < 13 ? meridian : "PM";
-  hour = hour < 13 ? "0" + hour : hour;
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
   hour = hour < 13 ? hour : hour - 12;
-  document.getElementById("hours").innerHTML = hour;
-  document.getElementById("minutes").innerHTML = minutes;
-  document.getElementById("seconds").innerHTML = seconds;
+  hour = hour < 10 ? "0" + hour : hour;
+
+  document.getElementById("date").innerHTML = month + " " + date;
+  document.getElementById("time").innerHTML =
+    hour + " : " + minutes + " : " + seconds;
+  // document.getElementById("hours").innerHTML = hour;
+  // document.getElementById("minutes").innerHTML = minutes;
+  // document.getElementById("seconds").innerHTML = seconds;
   document.getElementById("meridian").innerHTML = meridian;
 }
 showTime();
